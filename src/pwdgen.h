a04ca5e99617c20f5f4de9fffff55314cd569d07
JackTench <79285604+JackTench@users.noreply.github.com>
2024-04-25 21:39:29 +0100

    Remove C++ source files.

--- src/pwdgen.h
+++
@@ -1,9 +0,0 @@
-#ifndef PWDGEN_H
-#define PWDGEN_H
-
-#include <string>
-
-std::string generateServicePassword(int length);
-std::string generateHumanPassword();
-
-#endif
--- src/specs.h
+++
@@ -1,9 +0,0 @@
-#ifndef SPECS_H
-#define SPECS_H
-
-void getOS();
-void getCPU();
-void getRAM();
-void getSpecs();
-
-#endif
